63bf0490fe0f22d421eee9a6ef63b16f797cd6d8
venkateshellur <venkatesh.ellur@gmail.com>
2025-03-01 12:32:28 -0600

    Building reusable components and passing props simple example

--- 03_ReactEssentials/01-starting-project/src/App.jsx
+++ 03_ReactEssentials/01-starting-project/src/App.jsx
@@ -1,6 +1,34 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+
 const reactDescription = ["Fundamental", "Crucial", "Core"];
+const description = reactDescription[genRandomInt(2)];
+
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.src} alt="..."></img>
+      <h3>{props.title}</h3>
+      <p> {props.description}</p>
+    </li>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
 }
 
 function App() {
@@ -8,22 +36,34 @@ function App() {
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              src={componentsImg}
+              title="Components"
+              description="The Core UI building block."
+            ></CoreConcept>
+            <CoreConcept
+              src={componentsImg}
+              title=""
+              description=""
+            ></CoreConcept>
+            <CoreConcept
+              src={componentsImg}
+              title=""
+              description=""
+            ></CoreConcept>
+            <CoreConcept
+              src={componentsImg}
+              title=""
+              description=""
+            ></CoreConcept>
+          </ul>
+        </section>
       </main>
     </div>
   );
 }
 
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescription[genRandomInt(2)]} React concepts you will need for
-        almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
 export default App;
