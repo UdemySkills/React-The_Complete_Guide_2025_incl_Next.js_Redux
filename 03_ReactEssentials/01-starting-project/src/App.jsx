caf31ce294a15f352411600fd9c3a6dc4ee70cfa
venkateshellur <venkatesh.ellur@gmail.com>
2025-02-28 22:13:47 -0600

    Render dynamic test using array and Math Random Function

--- 03_ReactEssentials/01-starting-project/src/App.jsx
+++ 03_ReactEssentials/01-starting-project/src/App.jsx
@@ -1,3 +1,8 @@
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function App() {
   return (
     <div>
@@ -15,8 +20,8 @@ function Header() {
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescription[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   );
