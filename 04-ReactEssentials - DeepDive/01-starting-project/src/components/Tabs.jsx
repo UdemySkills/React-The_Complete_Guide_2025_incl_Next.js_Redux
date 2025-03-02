7f3a2da631c43dccbd97003cc804fbafe33ed4a9
venkateshellur <venkatesh.ellur@gmail.com>
2025-03-02 16:57:44 -0600

    Sec4 - 68. Passing the Element type dynamically as prop and using it accordingly

--- 04-ReactEssentials - DeepDive/01-starting-project/src/components/Tabs.jsx
+++ 04-ReactEssentials - DeepDive/01-starting-project/src/components/Tabs.jsx
@@ -1,7 +1,11 @@
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, buttonsContainer }) {
+  const ButtonsContainer = buttonsContainer; // React understands that the set value would be used as a component
+
   return (
     <>
-      <menu>{buttons}</menu>
+      {/*   React understands that you have the element type dynamic using props and constant mentioned above and behaves accordingly 
+      React is even smart enough to understand if you are using built in element or custom component based on the props value.*/}
+      <ButtonsContainer>{buttons}</ButtonsContainer>
       {children}
     </>
   );
