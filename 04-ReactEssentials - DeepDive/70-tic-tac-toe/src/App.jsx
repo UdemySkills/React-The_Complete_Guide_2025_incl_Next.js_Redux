e4a0fabadb52cf1f6460290f40345b373604f731
venkateshellur <venkatesh.ellur@gmail.com>
2025-03-02 20:50:16 -0600

    Sec4 - 78 - Update state by parameter (Toggle) - Not Instant - Not best Practice.

--- 04-ReactEssentials - DeepDive/70-tic-tac-toe/src/App.jsx
+++ 04-ReactEssentials - DeepDive/70-tic-tac-toe/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./Components/Player";
+import "./index.css";
 
 function App() {
   return (
--- 04-ReactEssentials - DeepDive/70-tic-tac-toe/src/Components/Player.jsx
+++ 04-ReactEssentials - DeepDive/70-tic-tac-toe/src/Components/Player.jsx
@@ -4,18 +4,26 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
+    // Never use the inverse of the value just like that when playing around with states.
+    // Use arrow function as it will be instant if done without arrow function it still works but will not be instant
+    // with below example when run we expect no change in the edit button text, but that is not the behavior as the execution is not instant.
     setIsEditing(!isEditing);
+    setIsEditing(!isEditing);
+
+    if (isEditing) {
+    }
   }
 
   let playerName = <span className="player-name">{name}</span>;
+
   if (isEditing) {
-    playerName = <input type="text"></input>;
+    playerName = <input type="text" value={name} required />;
   }
 
   return (
     <>
       <li>
-        <span className="=Player">
+        <span className="=player">
           {playerName}
           <span className="player-symbol">{symbol}</span>
         </span>
