83b49ab49c7c9733004ebec28a2b557305e9c94d
venkateshellur <venkatesh.ellur@gmail.com>
2025-03-02 19:36:09 -0600

    Sec4 - 75 - Making Player field editable - Self Try

--- 04-ReactEssentials - DeepDive/70-tic-tac-toe/src/Components/Player.jsx
+++ 04-ReactEssentials - DeepDive/70-tic-tac-toe/src/Components/Player.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
   return (
     <>
       <li>
         <span className="=Player">
-          <span className="player-name">{name}</span>
+          {isEditing ? (
+            <input type="text"></input>
+          ) : (
+            <span className="player-name">{name}</span>
+          )}
+
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
       </li>
     </>
   );
