12c026c45e2ff12007dd8888067389e98953d5d2
venkateshellur <venkatesh.ellur@gmail.com>
2025-03-18 17:05:12 -0500

    Sec5 - 99. Investment Calculator - Adding All Input Fields in single component

--- 05 Investment Calculator/01-starting-project/src/App.jsx
+++ 05 Investment Calculator/01-starting-project/src/App.jsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <UserInput></UserInput>
+    </>
+  );
 }
 
 export default App;
