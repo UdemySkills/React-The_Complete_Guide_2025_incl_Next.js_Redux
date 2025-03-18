13a148179f855bb1ee3c521210b23d45a0ee7d0c
venkateshellur <venkatesh.ellur@gmail.com>
2025-03-18 18:27:53 -0500

    Sec5 - 103. Investment Calculator - display Results in List including couple other derived values

--- 05 Investment Calculator/01-starting-project/src/components/Results.jsx
+++ 05 Investment Calculator/01-starting-project/src/components/Results.jsx
@@ -1,7 +1,41 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({ input }) {
   const resultData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultData[0].valueEndOfYear -
+    resultData[0].interest -
+    resultData[0].annualInvestment;
   console.log(input);
-  return <>resultData.map()</>;
+  return (
+    <>
+      <table id="result">
+        <thead>
+          <td>Year</td>
+          <td>Investment Value</td>
+          <td>Interest (Year) </td>
+          <td>Total Interest</td>
+          <td>Invested Capital</td>
+        </thead>
+        <tbody>
+          {resultData.map((yearData) => {
+            const totalInterest =
+              yearData.valueEndOfYear -
+              yearData.annualInvestment * yearData.year -
+              initialInvestment;
+            const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+            return (
+              <tr key={yearData.year}>
+                <td>{yearData.year}</td>
+                <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                <td>{formatter.format(yearData.interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalAmountInvested)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
