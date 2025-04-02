a707398150e5958c3ce285654a04105f0a678daf
venkateshellur <venkatesh.ellur@gmail.com>
2025-04-02 15:35:36 -0500

    Sec8 - 132. Refs and Portals - Repetition Userinput with State (two way binding)

--- 08_Refs and Portals/01-starting-project/src/components/Player.jsx
+++ 08_Refs and Portals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,22 @@
+import React, { useState, useContext } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("unknown entity");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+    setSubmitted(false);
+  }
+  function onSubmit() {
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : "Unknown"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleNameChange} value={playerName} />
+        <button onClick={() => onSubmit()}>Set Name</button>
       </p>
     </section>
   );
